9a05233278ae9dbb33e10142781ee938c049945c
Vashishtha Gautam <vashishtha.gautam09@gmail.com>
2021-05-18 23:54:50 +0530

    adding files to git hub pages

---
+++ build/precache-manifest.dd8b5d9d78b9b76e7a8a77a668d140ce.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a51b61aa8c2812906486555e8b6cce1",
+    "url": "/keep-app-Reactjs/index.html"
+  },
+  {
+    "revision": "265c582d559a233ba0b4",
+    "url": "/keep-app-Reactjs/static/js/2.0d2c8947.chunk.js"
+  },
+  {
+    "revision": "1aa3e4d17a181107f5f1",
+    "url": "/keep-app-Reactjs/static/js/main.d2e79250.chunk.js"
+  },
+  {
+    "revision": "aa5b2dbd40a312563eeb",
+    "url": "/keep-app-Reactjs/static/js/runtime-main.08741890.js"
+  }
+]);
